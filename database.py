5694d638c8e64c02595a0680fa5e60e9622d2ac0
msghoush <145580347+msghoush@users.noreply.github.com>
2026-02-16 22:55:25 +0300

    Refactor database connection and session setup
    Updated database connection to use environment variable for DATABASE_URL and improved session configuration.

--- database.py
+++ database.py
@@ -1,10 +1,15 @@
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, declarative_base
+import os
 
-DATABASE_URL = "sqlite:///./tis.db"
+# Get DATABASE_URL from environment variable
+DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
-SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
+# Create engine
+engine = create_engine(DATABASE_URL)
 
+# Session
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# Base
 Base = declarative_base()
