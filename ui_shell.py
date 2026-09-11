ae153c9a403985f372d2be1bb447bcc029fbb764
Mohamad El Ghoche <145580347+msghoush@users.noreply.github.com>
2026-09-11 23:54:18 +0300

    feat: brand Talent module navigation with Ghars logo

--- ui_shell.py
+++ ui_shell.py
@@ -358,6 +358,7 @@ def _build_nav_items(
             "label": "Talent & Potential",
             "href": "/talent",
             "icon": "sparkles",
+            "brand_logo": "img/talent-ghars-logo.svg",
             "permission_keys": ("talent_programs.view", "talent_evaluation_plans.view", "talent_assessments.view", "talent_review_candidates.view", "talent_learner_profiles.view", "talent_analytics.view"),
             "permission_mode": "any",
         },
